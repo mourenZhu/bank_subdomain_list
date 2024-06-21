003b8485811eef3ad0ffd57ee7b655dd3393c904
mourenZhu <zhumouren0623@qq.com>
2024-06-21 16:06:07 +0800

    1、增加子域名文件 2、对ip和子域名进行排序 3、优化代码 4、github actions 每日定时执行

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ import socket
 import os
 
 
-dest_dir_name = "bank_ips"
+ip_dest_dir_name = "bank_ips"
+domain_dest_dir_name = "bank_domains"
 
 
-def query_crt_sh(domain):
-    url = f"https://crt.sh/?q={domain}&output=json"
+def query_crt_sh(_domain):
+    url = f"https://crt.sh/?q={_domain}&output=json"
     response = requests.get(url)
     try:
         return [result['name_value'] for result in response.json()]
@@ -15,48 +16,72 @@ def query_crt_sh(domain):
         return []
 
 
-def get_domain_ips(root_domain: str):
-    domain_list = set()
-    sub_domain_list = query_crt_sh(root_domain)
-    for sub_domain in sub_domain_list:
-        domain_list_ = str(sub_domain).splitlines()
-        for domain_ in domain_list_:
-            if "*" not in domain_:
-                domain_list.add(domain_)
+def get_sub_domain_list(_root_domain: str):
+    print("正在获取 {} 的子域名".format(_root_domain))
+    _domain_set = set()
+    _sub_domain_list = query_crt_sh(_root_domain)
+    for _sub_domain in _sub_domain_list:
+        """
+        下面这一行代码是因为crt.sh获取子域名时，一个list[i]中有多行子域名
+        """
+        _real_domain_list = str(_sub_domain).splitlines()
+        for _real_sub_domain in _real_domain_list:
+            if "*" not in _real_sub_domain:
+                _domain_set.add(_real_sub_domain)
 
-    print("{} 子域名数为: {}".format(root_domain, len(domain_list)))
-    ip_set = set()
-    for domain_ in domain_list:
-        # print("ipset add icbc {}".format(icbc_domain))
+    _domain_list = list(_domain_set)
+    _domain_list.sort()
+    print("{} 子域名数为: {}".format(_root_domain, len(_domain_list)))
+    return _domain_list
+
+
+def get_ip_list(_domain_list: [str]):
+    _ip_set = set()
+    for _domain in _domain_list:
         try:
-            ip = socket.gethostbyname(domain_)
-            ip_set.add(ip)
+            ip = socket.gethostbyname(_domain)
+            _ip_set.add(ip)
         except Exception as e:
             pass
 
-    print("ip数为: {}".format(len(ip_set)))
-    return ip_set
+    print("ip数为: {}".format(len(_ip_set)))
+    _ip_list = list(_ip_set)
+    _ip_list.sort()
+    return _ip_list
 
 
 def get_bank_root_domains() -> [str]:
     domains_file_path = os.path.join(os.getcwd(), "bank_root_domains.txt")
     with open(domains_file_path, 'r') as f:
-        return set(f.read().splitlines())
+        root_domains = list(set(f.read().splitlines()))
+        root_domains.sort()
+        return root_domains
 
 
 def make_dest_dir():
-    dir_path = os.path.join(os.getcwd(), dest_dir_name)
+    dir_path = os.path.join(os.getcwd(), ip_dest_dir_name)
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
+    dir_path = os.path.join(os.getcwd(), domain_dest_dir_name)
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+
+
+def write_list_to_file(_dir_name: str, _domain: str, _list: [str]):
+    _file_path = os.path.join(os.getcwd(), _dir_name, '{}.txt'.format(_domain))
+    with open(_file_path, 'w') as f:
+        f.write('\n'.join(_list))
+        f.flush()
 
 
 if __name__ == "__main__":
     root_domain_list = get_bank_root_domains()
     make_dest_dir()
     for root_domain in root_domain_list:
-        ip_list = get_domain_ips(root_domain)
-        bank_ips_file_path = os.path.join(os.getcwd(), dest_dir_name, '{}.txt'.format(root_domain))
-        with open(bank_ips_file_path, 'w') as f:
-            f.write('\n'.join(ip_list))
-            f.flush()
+        sub_domain_list = get_sub_domain_list(root_domain)
+        write_list_to_file(domain_dest_dir_name, root_domain, sub_domain_list)
+
+        ip_list = get_ip_list(sub_domain_list)
+        write_list_to_file(ip_dest_dir_name, root_domain, ip_list)
+
 
